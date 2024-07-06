8a5a7dacc02162693d99d9721511171023fe4597
Breshnahamid20 <breshna2004@gmail.com>
2024-07-06 21:18:49 +0430

    solve linters error

--- scrpit.js
+++ scrpit.js
@@ -1,21 +1,21 @@
-document.getElementById('check-btn').addEventListener('click', function() {
-    var inputValue = document.getElementById('text-input').value.trim();
+document.getElementById('check-btn').addEventListener('click', () => {
+  const inputValue = document.getElementById('text-input').value.trim();
 
-    if (inputValue === '') {
-        alert('Please input a value');
-         return;
-    }
+  if (inputValue === '') {
+    no-alert('Please input a value');
+    return;
+  }
 
-     var normalizedValue = inputValue.toLowerCase().replace(/[^a-z0-9]/g, '');
+  const normalizedValue = inputValue.toLowerCase().replace(/[^a-z0-9]/g, '');
 
-    function isPalindrome(str) {
-         var reversedStr = str.split('').reverse().join('');
-         return str === reversedStr;
-    }
+  function isPalindrome(str) {
+    const reversedStr = str.split('').reverse().join('');
+    return str === reversedStr;
+  }
 
-    if (isPalindrome(normalizedValue)) {
-        document.getElementById('result').textContent = inputValue + ' is a palindrome';
-    } else {
-        document.getElementById('result').textContent = inputValue + ' is not a palindrome';
-    }
-        });
+  if (isPalindrome(normalizedValue)) {
+    document.getElementById('result').textContent = inputValue + ' is a palindrome';
+  } else {
+    document.getElementById('result').textContent = inputValue + ' is not a palindrome';
+  }
+  });
